978ed6dca9ee1f6d2322f803172dc13be6480220
Jan Gajdos <yeezuz332@gmail.com>
2025-03-09 22:18:41 +0100

    Oprava konfigurace pro ES moduly a GlassNavigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
     "./index.html",
@@ -70,5 +70,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [await import("tailwindcss-animate")],
 } 
